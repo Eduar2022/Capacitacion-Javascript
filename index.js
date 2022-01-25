be9260fcd9c61d112d8ae466572939ef80d62ea0
Eduard Sanchez <eduar-sanchez02@hotmail.com>
2022-01-24 19:10:46 -0500

    EA-Test: Pruebas en Javascript

---
+++ index.js
@@ -0,0 +1,16 @@
+var miVariable = 'variable'
+miVariable = 'otro valor'
+
+
+let variable = 'varible creada con let'
+variable = 'otro valor a let'
+
+const constante = 'este valor es constante'
+constante = 'esta operacion es invalida';
+console.log(constante)
+
+console.log(miVariable,variable)
+
+
+//no se va a hacer uso de "var"
+//solo declarar variables con "let" y constantes con "const"
